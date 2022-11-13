a5b20444b42839e225c1530649e8de28dcd2c26b
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2022-11-13 19:19:00 +0330

    disable css process url

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .vue({ version: 2 })
     .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        processCssUrls: false,
+    })
     .sourceMaps();
